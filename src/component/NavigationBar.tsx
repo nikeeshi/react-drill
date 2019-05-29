38af952dd6a28d787939633ddec02abc661c8697
nikeeshi <kenta.kf@gmail.com>
2019-05-29 23:15:24 +0900

    Fixed the link bug.

--- src/component/NavigationBar.tsx
+++ src/component/NavigationBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "styled-components/macro";
-import * as types from "styled-components/cssprop";
+// import * as types from "styled-components/cssprop";
 import styled from "styled-components/macro";
 const NavLink = styled.a`
   padding: 0 10px;
@@ -8,7 +8,7 @@ const NavLink = styled.a`
 const NavigationBar: React.FC = () => {
   return (
     <div>
-      <NavLink href="/">React drill</NavLink>
+      <NavLink href="./">React drill</NavLink>
       <NavLink href="https://github.com/nikeeshi/react-drill">Github</NavLink>
       <NavLink href="http://nikeeshipro.hatenablog.com/">My blog</NavLink>
     </div>
--- src/component/Styled.tsx
+++ src/component/Styled.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "styled-components/macro";
-import * as types from "styled-components/cssprop";
+// import * as types from "styled-components/cssprop";
 
 const Styled = () => <h1 css="color: papayawhip">Hello css prop</h1>;
 
